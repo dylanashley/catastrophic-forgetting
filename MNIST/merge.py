1aedf727dca2539ae6db9166348d9bdfcb85e544
Dylan Ashley <dylanashley@dylanashley.io>
2019-10-01 15:29:28 -0600

    fixed bug in merge file

--- MNIST/merge.py
+++ MNIST/merge.py
@@ -6,12 +6,12 @@ import glob
 import json
 import os
 
-filename = datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat() + '.json'
-assert(not os.path.isfile(filename))
+outfile = datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat() + '.json'
+assert(not os.path.isfile(outfile))
 data = []
-for filename in glob.glob('*.json'):
-    with open(filename, 'r') as infile:
-        data.append(json.load(infile))
-with open(filename, 'w') as outfile:
-    json.dump(data, outfile)
-print(filename)
+for infile in glob.glob('*.json'):
+    with open(infile, 'r') as f:
+        data.append(json.load(f))
+with open(outfile, 'w') as f:
+    json.dump(data, f)
+print(outfile)
